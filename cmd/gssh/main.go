e1e6a2f77f9e52b63f6fab03a5c7b1b0958b8f90
bigbyto <bigbyto@gmail.com>
2025-03-21 01:06:54 +0800

    feat: version info

--- cmd/gssh/main.go
+++ cmd/gssh/main.go
@@ -4,10 +4,13 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/xingty/rcode-go/gcode/config"
 	"github.com/xingty/rcode-go/gcode/ssh"
 )
+
+var version = "0.0.10"
 
 func main() {
 	for _, arg := range os.Args[1:] {
@@ -19,10 +22,17 @@ func main() {
 
 	var host string
 	var port int
+	var v bool
 
 	flag.StringVar(&host, "host", "127.0.0.1", "IPC server host")
 	flag.IntVar(&port, "port", 7532, "IPC server port")
+	flag.BoolVar(&v, "v", false, "Show version")
 	flag.Parse()
+
+	if v {
+		fmt.Printf("gssh version: %s %s/%s\n", version, runtime.GOOS, runtime.GOARCH)
+		os.Exit(0)
+	}
 
 	config.InitGCodeEnv()
 	ssh.Run(host, port, flag.Args())
